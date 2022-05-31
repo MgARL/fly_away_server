9771780bf5ff0f02e77cdafd9588e611623632f5
Miguel-Ubuntu-lap <mgarlcr@gmail.com>
2022-05-30 20:37:05 -0400

    Removed redundant delete routes, added Update Routes for Airline, Flight and Seat

--- controllers/admin_controller.js
+++ controllers/admin_controller.js
@@ -55,17 +55,17 @@ admin.post('/add-seats', async (req, res) =>{
   }
 })
 
-// Delete Routes
-admin.delete('/remove-user', async (req, res) => {
-  const { user_id } = req.body
+// Update Routes
+admin.put('/update-airline', async (req,res) =>{
+  const { airline_name, airline_id } = req.body
   try {
-    await User.destroy({
-      where: {
-        user_id
+    Airline.update({ airline_name }, {
+      where:{
+        airline_id
       }
     })
     res.status(200).json({
-      message: 'Successfully Deleted'
+      message: `Name has been changed to ${airline_name}`
     })
   } catch (error) {
     res.status(500).json({
@@ -75,13 +75,52 @@ admin.delete('/remove-user', async (req, res) => {
   }
 })
 
-admin.delete('/remove-user-reservations', async (req, res) => {
+admin.put('/update-flight', async (req,res) => {
+  const { flight_id, ...rest } = req.body
+  try {
+    await Flight.update({ ...rest }, {
+      where: {
+        flight_id
+      }
+    })
+    res.status(200).json({
+      message: `Flight Has been Updated`
+    })
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: 'Something Went Wrong!'
+    })
+  }
+})
+
+admin.put('/update-seat', async (req,res) => {
+  const { seat_id, ...rest } = req.body
+  try {
+      await Seat.update({...rest}, {
+      where:{
+        seat_id
+      }
+    })
+    res.status(200).json({
+      message: `Seat has been updated`
+    })
+  } catch (error) {
+    res.status(500).json({
+      error,
+      message: 'Something Went Wrong!'
+    })
+  }
+})
+
+// Delete Routes
+admin.delete('/remove-user', async (req, res) => {
   const { user_id } = req.body
   try {
-    await Reservation.destroy({
+    await User.destroy({
       where: {
         user_id
-      }  
+      }
     })
     res.status(200).json({
       message: 'Successfully Deleted'
@@ -113,48 +152,10 @@ admin.delete('/remove-flight', async (req,res) =>{
   }
 })
 
-admin.delete('/remove-flight-seats', async (req,res) =>{
-  const { flight_id } = req.body
-  try {
-    await Seat.destroy({
-      where:{
-        flight_id
-      }
-    })
-    res.status(200).json({
-      message: 'Successfully Deleted'
-    })
-  } catch (error) {
-    res.status(500).json({
-      error,
-      message: 'Something Went Wrong!'
-    })
-  }
-})
-
 admin.delete('/remove-airline', async (req,res) =>{
   const { airline_id } = req.body
   try {
     await Airline.destroy({
-      where: {
-        airline_id
-      }
-    })
-    res.status(200).json({
-      message: 'Successfully Deleted'
-    })
-  } catch (error) {
-    res.status(500).json({
-      error,
-      message: 'Something Went Wrong!'
-    })
-  }
-})
-
-admin.delete('/remove-airline-flights', async (req,res) =>{
-  const { airline_id } = req.body
-  try {
-    await Flight.destroy({
       where: {
         airline_id
       }
